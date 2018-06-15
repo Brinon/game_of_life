3889181d89282d2ad98482e31aaeeb00f78fefef
Pablo Llueca <pau@crowdai.com>
2018-06-15 16:22:15 +0200

    initial commit in ui

--- ui.py
+++ ui.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pygame
 from pygame import Rect, Surface
 """
@@ -44,8 +46,11 @@ class UI:
         self.OFFSET_TOP + self.height + self.OFFSET_CENTER + self.SCORES_HEIGHT +
         self.OFFSET_BOTTOM)
     self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-    self.game_screen = Surface((self.width, self.height))
-    self.scores = Surface((self.width, self.SCORES_HEIGHT))
+
+    self.components = []
+    game_screen = Surface((self.width, self.height))
+    scores = Surface((self.width, self.SCORES_HEIGHT))
+    self.components.append(game_screen, scores)
 
     self.cell_width = self.width / self.num_cols
     self.cell_height = self.height / self.num_rows
@@ -88,10 +93,14 @@ class UI:
     # inside the game
     return row, col
 
-  def draw(self, changed_cells=None):
+  def draw(self, **kwargs):
     """
     if changed_cells is None then paint all cells
     """
+    for component in self.components:
+      component.draw(**kwargs)
+      self.screen.blit(component, component.ui_rect)
+
     # self.screen.fill((255, 255, 255))
     self.scores.fill((255, 0, 0))
     if changed_cells is None:
@@ -117,13 +126,44 @@ class UIComponent(Surface):
   """ Component in the UI 
   """
 
-  def __init__(self, ui_rect, *args, **kwargs):
+  def __init__(self, ui_rect, *args, name=None, **kwargs):
     """
     Args:
       ui_rect: Rect indicating where in the ui window this component is placed
     """
-    super().__init__(*args, **kwargs)
+    super().__init__(ui_rect.size, *args, **kwargs)
     self.ui_rect = ui_rect
+    self.name = name or 'component_{}'.format(uuid.uuid4())
 
-  def draw(self):
+  def draw(self, **kwargs):
     raise NotImplementedError
+
+
+class GameUIComponent(UIComponent):
+
+  def __init__(self, num_cols, num_rows, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    self.num_cols = num_cols
+    self.num_rows = num_rows
+
+    self.cell_width = self.width // num_cols
+    self.cell_height = self.height // num_rows
+
+    active_rect = Surface((self.cell_width, self.cell_height))
+    active_rect.fill((255, 0, 0))
+    inactive_rect = Surface((self.cell_width, self.cell_height))
+    inactive_rect.fill((128, 128, 128))
+    self.surface_by_class = {0: inactive_rect, 1: active_rect}
+
+  def draw(self, changed_cells=None, **kwargs):
+    """ changed cells is a list of pairs [[position, class]]"""
+    if changed_cells is None:
+      changed_cells = [((i, j), 0) for i in range(self.num_cols) for j in range(self.num_rows)]
+
+    for (i, j) in changed_cells:
+      current_cell_rect = Rect(
+          (i * self.cell_width, j * self.cell_height),
+          (self.cell_width, self.cell_height),
+      )
+      self.blit(self.current_cell_surface((i, j)), current_cell_rect)
